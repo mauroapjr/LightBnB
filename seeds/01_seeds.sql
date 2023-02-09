1a18a40dcacc9e2a5d7a6a047a24f61beae5b792
mauroapjr <mauroapjr@outlook.com>
2023-02-08 22:29:05 -0500

    create queries

---
+++ seeds/01_seeds.sql
@@ -0,0 +1,5 @@
+-- INSERT INTO property_reviews
+-- (guest_id, property_id, reservation_id, rating, message)
+--    VALUES ('1', '1', '1', '3', 'mess'), ('2', '2', '2', '4', 'ok' ), ('3', '3', '3', '5', 'good');
+
+UPDATE property_reviews SET rating = '4' WHERE id = '6';
