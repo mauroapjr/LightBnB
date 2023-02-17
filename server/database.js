3b54b0d44720f3c488b5457eed02c5c4a7d2dd1f
mauroapjr <mauroapjr@outlook.com>
2023-02-17 15:50:45 -0500

    Updated addProperty function

--- server/database.js
+++ server/database.js
@@ -120,6 +120,7 @@ exports.getAllReservations = getAllReservations;
 const getAllProperties = function (options, limit = 10) {
   
   const queryParams = [];
+  const queryString = [];
 
   let sql = `
   SELECT properties.*, avg(property_reviews.rating) as average_rating
@@ -164,17 +165,50 @@ const getAllProperties = function (options, limit = 10) {
 
 exports.getAllProperties = getAllProperties;
 
-/**
+/**An object containing all of the property details.
  * Add a property to the database
- * @param {{}} property An object containing all of the property details.
+ * @param {{}} property 
  * @return {Promise<{}>} A promise to the property.
  */
-const addProperty = function (property) {
-  const propertyId = Object.keys(properties).length + 1;
-  property.id = propertyId;
-  properties[propertyId] = property;
-  return Promise.resolve(property);
+const addProperty = function(property) {
+  return db.query(`
+    INSERT INTO properties (
+      owner_id,
+      title,
+      description,
+      thumbnail_photo_url,
+      cover_photo_url,
+      cost_per_night,
+      street,
+      city,
+      province,
+      post_code,
+      country,
+      parking_spaces,
+      number_of_washrooms,
+      number_of_bedrooms,
+      active)
+    VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15)
+    RETURNING *;
+  `,[
+    property.owner_id,
+    property.title,
+    property.description,
+    property.thumbnail_photo_url,
+    property.cover_photo_url,
+    property.cost_per_night,
+    property.street,
+    property.city,
+    property.province,
+    property.post_code,
+    property.country,
+    property.parking_spaces,
+    property.number_of_bathrooms,
+    property.number_of_bedrooms,
+    true
+  ]).then(resp => resp.rows[0]);
 };
 exports.addProperty = addProperty;
+
 
 
